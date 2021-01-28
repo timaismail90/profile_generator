18da83e2d60e1662633c252c8ad79c79def8933a
Fatuma Ismail <44118528+timaismail90@users.noreply.github.com>
2021-01-28 08:46:43 -0600

    used async await and promise to ask multiple questions using Node's readline module

--- survey.js
+++ survey.js
@@ -1,12 +1,47 @@
-const readline = require('readline');
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// rl.question('What do you think of Node.js? ', (answer) => {
+//   console.log(`Thank you for your valuable feedback: ${answer}`);
+
+//   rl.close();
+// });
+
+'use strict'
+
+const readline = require('readline')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
+})
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+const question1 = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('what is your name? Nicknames are acceptable ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`)
+      resolve()
+    })
+  })
+}
 
-  rl.close();
-});
+const question2 = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('What is an activity you like doing? ', (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`)
+      resolve()
+    })
+  })
+}
+
+const main = async () => {
+  await question1()
+  await question2()
+  rl.close()
+}
+
+main()
